3f870a86a64881b81b852415338dabf526ec00eb
Yury Antonov <yantonov@yandex.ru>
2019-05-23 09:58:53 +0500

    replace user with root

--- python3/files/init-jupiter.sh
+++ python3/files/init-jupiter.sh
@@ -5,6 +5,6 @@
 jupyter notebook --generate-config
 
 # jupiter configuration
-echo "c.NotebookApp.ip = '*'"               >> ${HOME}/.jupyter/jupyter_notebook_config.py
-# echo "c.NotebookApp.port = 8888" >> ${HOME}/.jupyter/jupyter_notebook_config.py
-echo "c.NotebookApp.token = ''"             >> ${HOME}/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.ip = '*'"               >> /root/.jupyter/jupyter_notebook_config.py
+# echo "c.NotebookApp.port = 8888" >> /root/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.token = ''"             >> /root/.jupyter/jupyter_notebook_config.py
